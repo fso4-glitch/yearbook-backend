b856853576d1a8b3207d54ab363a2b513b54449d
fso4-glitch <fso4@aluno.ifnmg.edu.br>
2026-05-12 19:43:37 +0000

    feat: cria servidor express

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.json({ mensagem: 'Yearbook API está no ar! 🎓' });
+});
+
+if (process.env.VERCEL !== '1') {
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+  });
+}
+
+export default app;
