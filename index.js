353bdceabc147daa16b86a5840c2ac1b8185fdd9
fso4-glitch <fso4@aluno.ifnmg.edu.br>
2026-05-12 19:58:46 +0000

    fix: remove node_modules e .env do repositório

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ app.get('/', (req, res) => {
   res.json({ mensagem: 'Yearbook API está no ar! 🎓' });
 });
 
+app.get('/status', (req, res) => {
+  res.json({
+    status: 'ok',
+    timestamp: new Date()
+  });
+});
+
 if (process.env.VERCEL !== '1') {
   app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
